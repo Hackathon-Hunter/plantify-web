1333e058af3380b6e473dc620a9cdca6708c62d2
denandrapr <denandra1628@gmail.com>
2024-09-27 20:49:36 +0700

    feat(main): fix data main

--- src/app/profile/detail/page.tsx
+++ src/app/profile/detail/page.tsx
@@ -1,20 +1,47 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import { useSearchParams } from 'next/navigation';
+
 import Header from "@/components/Header";
 import DetailContent from "./partial/DetailContent";
+import DetailLocation from "./partial/DetailLocation";
 
+interface DataDetail {
+  images?: string;
+  names?: string;
+  prices?: string;
+  descriptions?: string;
+  locations?: string;
+  harvestTimes?: string;
+  harvestProfits?: string;
+  sizeAreas?: string;
+}
 
 export default function Marketplace() {
-  const [dataDetail, setDataDetail] = useState(null);
+  const searchParams = useSearchParams();
+  const [dataDetail, setDataDetail] = useState<DataDetail>();
+
   useEffect(() => {
-    const data = JSON.parse(localStorage.getItem("data_detail"));
-    setDataDetail(data);
-  }, [])
+    const datas: DataDetail = {
+      images: searchParams.get('image'),
+      names: searchParams.get('name'),
+      prices: searchParams.get('price'),
+      descriptions: searchParams.get('description'),
+      locations: searchParams.get('location'),
+      harvestTimes: searchParams.get('harvestTime'),
+      harvestProfits: searchParams.get('harvestProfit'),
+      sizeAreas: searchParams.get('sizeArea')
+    };
+
+    setDataDetail(datas);
+  }, [searchParams]);
+
   return (
     <div>
       <Header />
       <DetailContent dataDetail={dataDetail} />
+      <DetailLocation />
     </div>
   );
 }
